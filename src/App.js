c4516ecbe7f109fd7ec554254f918b4511699720
Naresh Rajput <nareshmewada014@gmail.com>
2023-01-24 08:25:06 +0530

    Tick Tick Game all functionality and UI done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Board from './Components/Board';
 function App() {
   return (
     <div className="App">
-      <h1>Let's See Who will win.</h1>
-      <h2>Slow vs Fast</h2>
+      <h1 className='title'>Let's See Who will win. 😎</h1>
+      <h2 className='title'>Slow vs Fast</h2>
       <Board />
     </div>
   );
